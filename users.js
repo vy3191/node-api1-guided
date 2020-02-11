fa7ec1853afa0295638ab2e526d014982d5ee7d8
Jason Maurer <jason@maur.co>
2020-02-11 13:52:38 -0700

    Make user IDs a number instead of a string

--- users.js
+++ users.js
@@ -1,5 +1,5 @@
 module.exports = [
-	{ id: "1", name: "Jane Doe" },
-	{ id: "2", name: "John Doe" },
-	{ id: "3", name: "Jack Doe" },
+	{ id: 1, name: "Jane Doe" },
+	{ id: 2, name: "John Doe" },
+	{ id: 3, name: "Jack Doe" },
 ]
